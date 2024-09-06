d8a6df0f506e175ca9806f7839dd381419111303
Xian Wang <wangxiantju@hotmail.com>
2024-09-05 23:42:29 -0400

    Update Sample_Code.ino

--- Sample_Code/Sample_Code.ino
+++ Sample_Code/Sample_Code.ino
@@ -1,18 +1,17 @@
-// This version starts out with printf() functionality that is not
-// available on the UNO and similar, but works on the Adafruit M0 boards.
-#include "RTClib.h"   // RTClib by Adafruit needed for %f in printf()
-
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);              // set the serial port speed
-  while(!Serial && millis() < 5000); // wait for the USB to wake up
-  Serial.printf("\n\nHello World\n\n");
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  Serial.println("Hello World!");
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  unsigned t = micros();
-  Serial.printf("%9u us since start, %10.6f Hello Again\n",
-                t, sin(t/10000000.));
-  delay(500);   // try to avoid using delay()
+  digitalWrite(13, 1);
+  Serial.print("Hello Again! ");
+  Serial.print("time is now ");
+  Serial.print(millis());
+  Serial.print(" ms from boot.\n");
+  delay(500);
+  digitalWrite(13, 0);
+  delay(500);
 }
